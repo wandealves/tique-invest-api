13124791f446503aba1db8b67ea1038ff7f694c6
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-05-08 14:03:16 -0300

    test: validação email correto

--- src/shared/email-validator-adapter.spec.ts
+++ src/shared/email-validator-adapter.spec.ts
@@ -1,5 +1,4 @@
 import validator from "validator";
-import isEmail from "validator/lib/isEmail";
 
 import { EmailValidatorAdapter } from "./email-validator";
 
@@ -12,13 +11,19 @@ jest.mock("validator", () => ({
 describe("EmailValidator Adapter", () => {
   test("Should return false if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
-    jest.spyOn(validator,'isEmail').mockReturnValueOnce(false)
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid_email@email.com");
     expect(isValid).toBe(false);
   });
-  test("Should return false if validator returns false", () => {
+  test("Should return true if validator returns false", () => {
     const sut = new EmailValidatorAdapter();
     const isValid = sut.isValid("invalid_email@email.com");
     expect(isValid).toBe(true);
+  });
+  test("Should call validator with correct email", () => {
+    const sut = new EmailValidatorAdapter();
+    sut.isValid("any_email@email.com");
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+    expect(isEmailSpy).toHaveBeenCalledWith("any_email@email.com");
   });
 });
