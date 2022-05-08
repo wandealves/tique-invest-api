321ef3da3d4f130818f8143a933de29f88b65cd6
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-05-08 16:26:10 -0300

    feat: criação da funcionalidade de criar account

--- src/repositories/usecases/create-account/create-account-protocols.ts
+++ src/repositories/usecases/create-account/create-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from "../../../domain/usecases/create-account";
 export * from "../../../domain/models/account";
 export * from "../../protocols/encrypter";
+export * from "../../protocols/create-account-repository";
---
+++ src/repositories/usecases/create-account/create-account.ts
@@ -0,0 +1,29 @@
+import {
+  CreateAccount,
+  CreateAccountModel,
+  AccountModel,
+  Encrypter,
+  CreateAccountRepository
+} from "./create-account-protocols";
+
+export class AccountRepository implements CreateAccount {
+  private readonly encrypter: Encrypter;
+  private readonly createAccountRepository: CreateAccountRepository;
+
+  constructor(
+    encrypter: Encrypter,
+    createAccountRepository: CreateAccountRepository
+  ) {
+    this.encrypter = encrypter;
+    this.createAccountRepository = createAccountRepository;
+  }
+
+  async create(accountData: CreateAccountModel): Promise<AccountModel | null> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    this.createAccountRepository.create({
+      ...accountData,
+      password: hashedPassword
+    });
+    return new Promise(resolve => resolve(null));
+  }
+}
