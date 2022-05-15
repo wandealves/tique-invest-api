5fab0b930db3d9343eac6efc6f70e2d4f46ca083
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-05-15 12:24:13 -0300

    test: cors

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import { setupApp } from "../config/app";
+
+describe("CORS Middleware", () => {
+  test("Should enable CORS", async () => {
+    const app = await setupApp();
+
+    app.post("/test_cors", (req, res) => {
+      res.send();
+    });
+
+    await request(app)
+      .get("/test_cors")
+      .expect("access-control-allow-origin", "*")
+      .expect("access-control-allow-headers", "*")
+      .expect("access-control-allow-methods", "*");
+  });
+});
