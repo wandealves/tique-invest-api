f9611010dd60569362a894fd7845ef802aa4777f
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-05-21 12:30:21 -0300

    feat: calculo total do ativo comprado

--- src/domain/models/purchased-asset.ts
+++ src/domain/models/purchased-asset.ts
@@ -1,11 +1,13 @@
+import { getMonth, getYear } from "date-fns";
+
 import { Asset } from "./asset";
 import { Investment } from "./investment";
-import { User } from "./user";
 
 export class PurchasedAsset {
   private readonly id: number;
   private readonly price: number;
   private readonly quantity: number;
+  private readonly total: number;
   private readonly date: Date;
   private readonly month: number;
   private readonly year: number;
@@ -18,19 +20,22 @@ export class PurchasedAsset {
     price: number,
     quantity: number,
     date: Date,
-    month: number,
-    year: number,
     asset: Asset,
     investment: Investment
   ) {
     this.id = id;
     this.price = price;
     this.quantity = quantity;
+    this.total = this.calculateTotal();
     this.date = date;
-    this.month = month;
-    this.year = year;
+    this.month = getMonth(this.date);
+    this.year = getYear(this.date);
 
     this.asset = asset;
     this.investment = investment;
+  }
+
+  public calculateTotal(): number {
+    return this.price * this.quantity;
   }
 }
