a2846476e8efa6070bf5d3b840dd70557af4e083
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-05-21 12:01:59 -0300

    feat: alteração do modelo de dados

--- prisma/migrations/20220521145726_init/migration.sql
+++ prisma/migrations/20220521145726_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "TypeAsset" AS ENUM ('ACAO', 'BDR', 'CDB', 'COE', 'FIIS', 'LCI', 'LCA', 'LC', 'MULTIMERCADO', 'TESOURODIRETO', 'OPCOES');
+CREATE TYPE "TypeInvestment" AS ENUM ('ACAO', 'BDR', 'CDB', 'COE', 'FIIS', 'LCI', 'LCA', 'LC', 'MULTIMERCADO', 'TESOURODIRETO', 'OPCOES');
 
 -- CreateTable
 CREATE TABLE "countries" (
@@ -24,20 +24,29 @@ CREATE TABLE "assets" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(20) NOT NULL,
     "code" VARCHAR(10) NOT NULL,
-    "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
-    "quantity" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "iconUrl" VARCHAR,
-    "type" "TypeAsset" NOT NULL,
-    "userId" INTEGER,
-    "investmentId" INTEGER,
 
     CONSTRAINT "assets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "purchasedAssets" (
+    "id" SERIAL NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "quantity" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "date" TIMESTAMP(3) NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "investmentId" INTEGER,
+
+    CONSTRAINT "purchasedAssets_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "investments" (
     "id" SERIAL NOT NULL,
     "total" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "type" "TypeInvestment" NOT NULL,
     "countryId" INTEGER NOT NULL,
     "userId" INTEGER,
 
@@ -56,11 +65,11 @@ CREATE UNIQUE INDEX "assets_code_key" ON "assets"("code");
 -- CreateIndex
 CREATE INDEX "assets_code_idx" ON "assets"("code");
 
--- AddForeignKey
-ALTER TABLE "assets" ADD CONSTRAINT "assets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- CreateIndex
+CREATE INDEX "investments_userId_idx" ON "investments"("userId");
 
 -- AddForeignKey
-ALTER TABLE "assets" ADD CONSTRAINT "assets_investmentId_fkey" FOREIGN KEY ("investmentId") REFERENCES "investments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "purchasedAssets" ADD CONSTRAINT "purchasedAssets_investmentId_fkey" FOREIGN KEY ("investmentId") REFERENCES "investments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "investments" ADD CONSTRAINT "investments_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "countries"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
