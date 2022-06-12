3deb2358c6a682db6e2718e4b25422bdbc4e899f
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-06-12 14:02:26 -0300

    test: adição do criar carteira de ativos

---
+++ tests/usecases/wallet/create-wallet.spec.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+
+import { CreateWalletDto } from "../../../src/usecases/dtos";
+import { CreateWallet } from "../../../src/usecases";
+import { ICreateWallet } from "../../../src/usecases/interfaces";
+import { createWalletDto } from "../../data/mock-data";
+import {
+  WalletRepositorySpy,
+  AssetRepositorySpy
+} from "../../infra/mocks/mock-repositories";
+
+interface SutTypes {
+  createWallet: ICreateWallet;
+  walletRepositorySpy: WalletRepositorySpy;
+  assetRepositorySpy: AssetRepositorySpy;
+  createWalletDto: CreateWalletDto;
+}
+
+const makeSut = (): SutTypes => {
+  const walletRepositorySpy = new WalletRepositorySpy();
+  const assetRepositorySpy = new AssetRepositorySpy();
+  const createWallet = new CreateWallet(
+    walletRepositorySpy,
+    assetRepositorySpy
+  );
+
+  return {
+    createWallet,
+    walletRepositorySpy,
+    assetRepositorySpy,
+    createWalletDto
+  };
+};
+
+describe("Usecase create wallet", () => {
+  test("Deve criar a cateira de ativos", async () => {
+    const { createWallet, createWalletDto } = makeSut();
+
+    const result = await createWallet.execute(createWalletDto);
+
+    if (result.isRight()) expect(result.value.id).toBe(1);
+  });
+});
