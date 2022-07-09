33d356e9f55fa7f4bb0e5de6d6f58b02697dc344
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2022-07-09 16:07:37 -0300

    feat: domino

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,21 +1,72 @@
 export class User {
-  private id: number;
-  private name: string;
-  private email: string;
-  private cpf: string;
-  private avatarUrl: string;
+  private _id: number;
+  private _name: string;
+  private _email: string;
+  private _cpf: string;
+  private _password: string;
+  private _avatarUrl: string;
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set cpf(value: string) {
+    this._cpf = value;
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  set avatarUrl(value: string) {
+    this._avatarUrl = value;
+  }
+
+  get avatarUrl(): string {
+    return this._avatarUrl;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
 
   constructor(
     id: number,
     name: string,
     email: string,
     cpf: string,
+    password: string,
     avatarUrl: string
   ) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.cpf = cpf;
-    this.avatarUrl = avatarUrl;
+    this._id = id;
+    this._name = name;
+    this._email = email;
+    this._cpf = cpf;
+    this._password = password;
+    this._avatarUrl = avatarUrl;
   }
 }
